24107647b778e928b9118b77810944efde5ae78d
Hitesh V <hiteshpratyush@gmail.com>
2018-06-06 02:54:02 +0530

    first commit

---
+++ websocket/websocket.ino
@@ -0,0 +1,50 @@
+#include <WiFi.h>
+#include <WebSocketServer.h>
+ 
+WiFiServer server(80);
+WebSocketServer webSocketServer;
+ 
+const char* ssid = "net";
+const char* password =  "password";
+
+void setup() {
+    Serial.begin(115200);
+    
+    WiFi.begin(ssid, password); 
+    
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.println("Connecting to WiFi..");
+    }
+    
+    Serial.println("Connected to the WiFi network");
+    Serial.println(WiFi.localIP());
+    
+    server.begin();
+delay(100);
+}
+ 
+void loop() {
+ 
+  WiFiClient client = server.available();
+ 
+  if (client.connected() && webSocketServer.handshake(client)) {
+ 
+    String data;      
+ 
+    while (client.connected()) {
+ 
+      data = webSocketServer.getData();
+ 
+      if (data.length() > 0) {
+         Serial.println(data);
+         webSocketServer.sendData(data);
+      }
+ 
+      delay(1); // Delay needed for receiving the data correctly
+   }
+ 
+   Serial.println("The client disconnected");
+  }
+ 
+}
